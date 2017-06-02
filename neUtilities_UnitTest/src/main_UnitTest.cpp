0ec53f2233f6af7b92a422188b1ffef4b8512b00
Alberto Flores <idv14b.aflores@uartesdigitales.edu.mx>
2017-06-01 20:07:19 -0500

    no message

--- neUtilities_UnitTest/src/main_UnitTest.cpp
+++ neUtilities_UnitTest/src/main_UnitTest.cpp
@@ -4,7 +4,17 @@
 
 
 namespace neEngineSDK {
-  TEST(Game, Math) {
+  TEST(Utilities, types) {
+    EXPECT_TRUE(sizeof(uint8) == 1);
+    EXPECT_TRUE(sizeof(uint16) == 2);
+    EXPECT_TRUE(sizeof(uint32) == 4);
+    EXPECT_TRUE(sizeof(uint64) == 8);
+    EXPECT_TRUE(sizeof(int8) == 1);
+    EXPECT_TRUE(sizeof(int16) == 2);
+    EXPECT_TRUE(sizeof(int32) == 4);
+    EXPECT_TRUE(sizeof(int64) == 8);
+  }
+  TEST(Utilities, Math) {
     ASSERT_TRUE(CMath::Power(2, 2) == 4);
     ASSERT_TRUE(CMath::Ceil(5.6f) == 6);
     ASSERT_TRUE(CMath::Sqrt(9) == 3);
